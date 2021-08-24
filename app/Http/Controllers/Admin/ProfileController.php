0c9cc9b140a4e8f675f4c0c85da7888b8c72911a
EC2 Default User <ec2-user@ip-172-31-44-54.us-east-2.compute.internal>
2021-08-24 05:11:24 +0000

    Laravel08

---
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProfileController extends Controller
+{
+    //
+    public function add()
+    {
+        return view('admin.profile.create');
+    }
+    
+    public function create(Request $request)
+    {
+        return redirect('admin/profile/create');
+    }
+    
+    public function edit(Request $request)
+    {
+        return view('admin.profile.edit');
+    }
+    
+    public function update(Request $request)
+    {
+        return redirect('admin/profile/edit');
+    }
+}
