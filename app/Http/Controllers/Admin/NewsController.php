b73d19db9502e34d418b2690fd3fe63440118cad
EC2 Default User <ec2-user@ip-172-31-44-54.us-east-2.compute.internal>
2021-09-04 12:24:56 +0000

    Laravel 14

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+// News Modelを使用
+use App\News;
+
 class NewsController extends Controller
 {
     //
@@ -15,6 +18,30 @@ class NewsController extends Controller
     
     public function create(Request $request)
     {
+        // Varidationを行う
+        $this->validate($request, News::$rules);
+        
+        $news = new News; //newsのデータベース
+        $form = $request->all();
+        
+        // フォームから画像が送信されてきたら、保存して、$news->image_pathに画像のパスを保存する
+        if (isset($form['image'])) {
+            $path = $request->file('image')->store('public/image'); //送られてきた画像をpublic/imageに保存; store()は画像ファイルを他と被らない名前を使って保存する
+            $news->image_path = basename($path); // databaseのimage_pathカラムに画像のパスを保存; database()で画像ファイルの名前だけ取り出す
+        } else {
+            $news->image_path = null;
+        }
+        
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        // フォームから送信されてきたimageを削除する
+        unset($form['image']);
+        
+        // データベースに保存する
+        $news->fill($form); // ここで本文とかタイトルとかをnewsに入れる
+        $news->save();
+        
+        
         // admin/news/createにリダイレクトする
         return redirect('admin/news/create');
     }
