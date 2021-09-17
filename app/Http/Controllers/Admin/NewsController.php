337c8f8de3ed986cea15ea995ebd43471c7433d7
EC2 Default User <ec2-user@ip-172-31-44-54.us-east-2.compute.internal>
2021-09-17 05:05:33 +0000

    Laravel 16

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -16,13 +16,13 @@ class NewsController extends Controller
         return view('admin.news.create');
     }
     
-    public function create(Request $request)
+    public function create(Request $request) // Requestはブラウザから送られるユーザーの情報全てを含んだオブジェクト。それを$request変数に代入して使う
     {
         // Varidationを行う
-        $this->validate($request, News::$rules);
+        $this->validate($request, News::$rules); // DEBUGBAR_ENABLED=null  # デフォルト。APP_DEBUGに応じて決まる
         
-        $news = new News; //newsのデータベース
-        $form = $request->all();
+        $news = new News; //newsのデータベースを$newsに入れる。
+        $form = $request->all(); //$requestの情報を全て取得
         
         // フォームから画像が送信されてきたら、保存して、$news->image_pathに画像のパスを保存する
         if (isset($form['image'])) {
@@ -44,5 +44,59 @@ class NewsController extends Controller
         
         // admin/news/createにリダイレクトする
         return redirect('admin/news/create');
+    }
+    
+    // 投稿したニュース一覧
+    public function index(Request $request)
+    {
+        $cond_title = $request->cond_title;
+        if ($cond_title != '') {
+        // 検索されたら検索結果を取得する
+        $posts = News::where('title', $cond_title)->get(); //　newsテーブルの中のtitleカラムで$cond_title（ユーザーが入力した文字）に一致するレコードをすべて取得することができます。
+        } else {
+            // それ以外はすべてのニュースを取得する
+            $posts = News::all();
+        }
+        return view('admin.news.index', ['posts' => $posts, 'cond_title' => $cond_title]); // index.blade.phpのファイルに取得したレコード（$posts）と、ユーザーが入力した文字列（$cond_title）を渡し、ページを開きます。
+        // ControllerではModelに対して where メソッドを指定して検索しています。
+    }
+    
+    public function edit(Request $request)
+    {
+        // News Modelからデータ取得
+        $news = News::find($request->id); // $newsにNewのidが一致する場所の情報代入
+        if (empty($news)) {
+            abort(404);
+        }
+        return view('admin.news.edit', ['news_form' => $news]);
+    }
+    
+    public function update(Request $request)
+    {
+        // Validationをかける
+        $this->validate($request, News::$rules);
+        // News Modelからデータを取得する
+        $news = News::find($request->id);
+        // 送信されてきたフォームデータを格納する
+        $news_form = $request->all();
+        if ($request->remove == 'true') {
+            $news_form['image_path'] = null;
+        } elseif ($request->file('image')) {
+            $path = $request->file('image')->store('public/image');
+            $news_form['image_path'] = basename($path);
+        } else {
+            $news_form['image_path'] = $news->image_path;
+        }
+        
+        
+        unset($news_form['image']);
+        unset($news_form['remove']);
+        unset($news_form['_token']);
+
+        
+        // 該当するデータを上書きして保存する
+        $news->fill($news_form)->save();
+        
+        return redirect('admin/news');
     }
 }
--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -15,21 +15,69 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
     
-    public function create(Request $request)
+    public function create(Request $request) // Requestはブラウザから送られるユーザーの情報全てを含んだオブジェクト。それを$request変数に代入して使う
     {
-        // Validation
-        $this->validate($request, Profile::$rules);
+        // Varidationを行う
+        $this->validate($request, Profile::$rules); // DEBUGBAR_ENABLED=null  # デフォルト。APP_DEBUGに応じて決まる
         
+        $profile = new Profile; //Profileのデータベースを$newsに入れる。
+        $form = $request->all(); //$requestの情報を全て取得
+        
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        
+        // データベースに保存する
+        $profile->fill($form); // ここで本文とかタイトルとかをnewsに入れる
+        $profile->save();
+        
+        
+        // admin/news/createにリダイレクトする
         return redirect('admin/profile/create');
+    }
+    
+    // 投稿したニュース一覧
+    public function index(Request $request)
+    {
+        $cond_title = $request->cond_title;
+        if ($cond_title != '') {
+        // 検索されたら検索結果を取得する
+        $posts = Profile::where('title', $cond_title)->get(); //　newsテーブルの中のtitleカラムで$cond_title（ユーザーが入力した文字）に一致するレコードをすべて取得することができます。
+        } else {
+            // それ以外はすべてのニュースを取得する
+            $posts = Profile::all();
+        }
+        return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]); // index.blade.phpのファイルに取得したレコード（$posts）と、ユーザーが入力した文字列（$cond_title）を渡し、ページを開きます。
+        // ControllerではModelに対して where メソッドを指定して検索しています。
     }
     
     public function edit(Request $request)
     {
-        return view('admin.profile.edit');
+        // Profile Modelからデータを取得する
+        $profile = Profile::find($request->id);
+        if (empty($profile)) {
+            abort(404);
+        }
+
+        return view('admin.profile.edit', ['profile_form' => $profile]);
     }
     
     public function update(Request $request)
     {
-        return redirect('admin/profile/edit');
+        // Validationをかける
+        $this->validate($request, Profile::$rules);
+        // Profile Modelからデータを取得する
+        $profile = Profile::find($request->id);
+        // 送信されてきたフォームデータを格納する
+        $profile_form = $request->all();
+        
+        unset($profile_form['remove']);
+        unset($profile_form['_token']);
+
+        // 該当するデータを上書きして保存する
+        $profile->fill($profile_form)->save();
+        
+        return redirect('admin/profile');
     }
+    
+
 }
